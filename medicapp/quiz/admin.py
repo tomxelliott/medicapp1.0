8a0f8f5189a94fdf09c715bbfb0a49b7fce57f0e
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-10-25 23:16:26 +0100

    Implementing question portal
    Building up the admin portal to add and remove questions and potential answers.

--- medicapp/quiz/admin.py
+++ medicapp/quiz/admin.py
@@ -1,6 +1,28 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    max_num = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,         {'fields': ['question']}),
+        ('Choices',    {'fields': ['choice_text', 'correct_answer']}),
+    ]
+
+    list_display = ('question', 'choice_text', 'correct_answer')
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
