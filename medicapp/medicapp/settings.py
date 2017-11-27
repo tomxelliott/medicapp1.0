418c64eb275dd02bf5fe07b746eadbd4d4f950b2
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-11-27 22:00:09 +0000

    Changes to settings.py to allow for Heroku DB setup
    DB persistence.

--- medicapp/medicapp/settings.py
+++ medicapp/medicapp/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -72,6 +73,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
