116fe0fc8c846bee085fb1ab5e88ab1c1e87afb5
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-11-28 09:54:41 +0000

    Settings changed for local and added emoji stylesheet to front end

--- medicapp/settings.py
+++ medicapp/settings.py
@@ -91,10 +91,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
